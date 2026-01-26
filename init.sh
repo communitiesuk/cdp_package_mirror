e45f40afa373988f9cb9bf7d4daede0ba5577f4a
Finn Wee Lip <finn.tan@test.communities.gov.uk>
2026-01-26 15:07:12 +0000

    fix init

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# set -e
+set -e
 
 # if "dev" environment, exit early - dev has no restrictions
-# if [ "$env" == "dev" ]; then
-#   exit 0
-# fi
+if [ "$env" == "dev" ]; then
+  exit 0
+fi
 
 # extract the access token from the json response - note jq not available
 export ACCESS_TOKEN=$(curl -X POST https://login.microsoftonline.com/$AZURE_TENANT_ID/oauth2/v2.0/token \
@@ -15,9 +15,9 @@ export ACCESS_TOKEN=$(curl -X POST https://login.microsoftonline.com/$AZURE_TENA
     | python3 -c 'import sys, json; print(json.load(sys.stdin)["access_token"].strip())')
 
 # exit 1 if access_token is empty
-# if [[ -z "$ACCESS_TOKEN" ]]; then
-#   exit 1
-# fi
+if [[ -z "$ACCESS_TOKEN" ]]; then
+  exit 1
+fi
 
 # add index url to default pip.conf
 cat <<EOF | sudo tee /etc/pip.conf
